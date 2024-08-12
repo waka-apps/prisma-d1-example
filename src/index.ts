03b43a2e0a7e80fd22426b052633a9ecd96ae350
waka-apps <waka.apps@gmail.com>
2024-08-12 12:15:01 +0900

    query

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { PrismaD1 } from '@prisma/adapter-d1';
+import { PrismaClient } from '@prisma/client';
+
+export interface Env {
+	DB: D1Database;
+}
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const adapter = new PrismaD1(env.DB);
+		const prisma = new PrismaClient({ adapter });
+
+		const users = await prisma.user.findMany();
+		const result = JSON.stringify(users);
+		return new Response(result);
 	},
 } satisfies ExportedHandler<Env>;
